50a72c720533b73a19c7672a900b12af1b4cbcf8
JAYRUS KIPROTICH <jayruskirui@gmail.com>
2026-05-20 16:48:28 +0300

    first commit

---
+++ src/Components/Weather.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className='search-bar'>
+          <input type="text" placeholder='Search' />
+        </div>
+    </div>
+  )
+}
+
+export default Weather
